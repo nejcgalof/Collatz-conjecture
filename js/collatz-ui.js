3bee129ef8acc3fadcdedcbc371c3eb4bb7a251e
nejc <galof.nejc@gmail.com>
2016-08-23 18:43:33 +0200

    add bootstrap, collatz, button and rangeslider work together now

---
+++ js/collatz-ui.js
@@ -0,0 +1,73 @@
+$(function() {
+  var timer = null,
+      level = 1,
+      max = 20,
+      duration = 1000,
+      clientWidth = $(window).width(),
+      clientHeight = $(window).height(),
+      //clientHeight = document.getElementById('content').clientHeight;
+      r = 700 / 2,
+      animation=false,
+      collatz = reverseCollatz(r, max);
+
+
+  var vis = d3.select("#vis")
+    .append("svg")
+      .attr("width", r * 2)
+      .attr("height", r * 2)
+    .append("g")
+      .attr("transform", "translate(" + r + "," + r + ")");
+
+  function plotLevel() {
+    if (level <= max) {
+      $('#level-val').text(level);
+      vis.call(collatz(level, duration));
+      if(animation){
+        level++;
+      }
+      else{
+        level=document.querySelector('output').textContent;
+        if (timer) clearInterval(timer);
+        vis.call(collatz(level, duration));
+      }
+    }
+  }
+/*
+  $('#play').click(function() {
+    if (level > max) level = 1;
+    plotLevel();
+    if (timer) clearInterval(timer);
+    timer = setInterval(function() {
+      if (level <= max) plotLevel();
+      else $('#stop').click();
+    }, duration);
+    $(this).hide();
+    $('#stop').show();
+  });
+/*
+  $('#stop').click(function() {
+    if (timer) clearInterval(timer);
+    $(this).hide();
+    $('#play').show();
+  });
+*/
+  $('#play').click(function() {
+    animation=false;
+    level=document.querySelector('output').textContent;
+    $('#level-val').text(level);
+    if (timer) clearInterval(timer);
+    vis.call(collatz(level, duration));
+  });
+
+  $('#animation').click(function() {
+      animation=true;
+      level=1;
+      if (level > max) level = 1;
+    plotLevel();
+    if (timer) clearInterval(timer);
+    timer = setInterval(function() {
+      if (level <= max) plotLevel();
+      else $('#stop').click();
+    }, duration);
+  }).click();
+});
